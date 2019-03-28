3aad291cc4f5057366f05dca44f42387dc943e98
p3t1k9 <45039684+p3t1k9@users.noreply.github.com>
2019-03-28 10:16:23 +0100

    Update Sum_Strings_as_Numbers.php

--- Sum_Strings_as_Numbers.php
+++ Sum_Strings_as_Numbers.php
@@ -1,5 +1,4 @@
-/*
-Given the string representations of two integers, return the string representation of the sum of those integers.
+/* Given the string representations of two integers, return the string representation of the sum of those integers.
 
 For example:
 
