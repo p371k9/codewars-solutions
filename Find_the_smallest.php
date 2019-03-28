8cec88e23d60d93d16fcfaf5767ae0670ea31beb
p3t1k9 <45039684+p3t1k9@users.noreply.github.com>
2019-03-28 10:15:38 +0100

    Update Find_the_smallest.php

--- Find_the_smallest.php
+++ Find_the_smallest.php
@@ -1,5 +1,4 @@
-/*
-You have a positive number n consisting of digits. You can do at most one operation: Choosing the index of a digit in the number, remove this digit at that index and insert it back to another place in the number.
+/* You have a positive number n consisting of digits. You can do at most one operation: Choosing the index of a digit in the number, remove this digit at that index and insert it back to another place in the number.
 
 Doing so, find the smallest number you can get.
 
